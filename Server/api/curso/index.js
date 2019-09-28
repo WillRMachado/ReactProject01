4745b088dc99b470d61fbb73fc9ff9ed73d131c8
WillRMachado <50939810+WillRMachado@users.noreply.github.com>
2019-09-28 18:09:58 -0300

    1
    1

--- Server/api/curso/index.js
+++ Server/api/curso/index.js
@@ -1,11 +1,12 @@
 //serviços rest
-const curso = require('./model');
-curso.methods(['get', 'post', 'put', 'delete']);
-curso.updateOptions({
-    new: true,
-    runValidators: true
+const curso = require('./model'); //seteando formato da DB no mongo e linkando com o node-restful
+curso.methods(['get', 'post', 'put', 'delete']); //usa o node-restfulç para dizer quais formas da API estao habilitadas
+curso.updateOptions({   //mais configurtacoes de permissao
+    new: true,  //permite criar
+    runValidators: true  // verifica com o modeel
 });
 module.exports = curso;
+
 
 
 //rafael@verzel.com.br
--- Server/loader.js
+++ Server/loader.js
@@ -1,5 +1,5 @@
-const server = require('./config/server')
+const server = require('./config/server') //coloca o server no ar
 const db = require('./config/db')
-const routes = require('./config/routes')
+const routes = require('./config/routes') //faz o node-restfull fununciar e o express
 
-routes(server)
+// routes(server)
